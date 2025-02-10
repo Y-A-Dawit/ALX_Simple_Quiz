bb7f18f2e53aae9655e76f2fc526d3739265ee35
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-02-10 23:01:29 +0300

    update

--- quiz.js
+++ quiz.js
@@ -1,15 +1,14 @@
 function checkAnswer() {
     const correctAnswer = 4; // represents the correct answer to the quiz question
     const radioButtons = document.querySelector('input[name="quiz"]:checked'); // a list that give a selected/clicked radio button
+    // if any of the options are selected, then....
+    const userAnswer = Number(radioButtons.value) // this one converts the value obtained from one of the input fields/radio button from string to integer
 
-    if (radioButtons) { // if any of the options are selected, then....
-        const userAnswer = Number(radioButtons.value) // this one converts the value obtained from one of the input fields/radio button from string to integer
-
-        if (userAnswer === correctAnswer ) { // if what the user selected is equal to the actual correct answer
-            document.querySelector('#feedback').textContent = "Correct! Well done."
-        } else {
-            document.querySelector('#feedback').textContent = "That's incorrect. Try again!"
-        }
+    // if what the user selected is equal to the actual correct answer
+    if ( userAnswer === correctAnswer ) {
+        document.querySelector('#feedback').textContent = "Correct! Well done."
+    } else {
+        document.querySelector('#feedback').textContent = "That's incorrect. Try again!"
     }
 }
 
