5d0e1e13b6c20bc1981ffb9da350c554320fc656
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-02-10 22:44:10 +0300

    ALX_Simple_Quiz

--- quiz.js
+++ quiz.js
@@ -1,3 +1,18 @@
 function checkAnswer() {
-    const correctAnswer = 4;
+    const correctAnswer = 4; // represents the correct answer to the quiz question
+    const radioButtons = document.querySelector('input[name="quiz"]:checked'); // a list that give a selected/clicked radio button
+
+    if (radioButtons) { // if any of the options are selected, then....
+        const userAnswer = Number(radioButtons.value) // this one converts the value obtained from one of the input fields/radio button from string to integer
+
+        if ( userAnswer === correctAnswer ) { // if what the user selected is equal to the actual correct answer
+            document.querySelector('#feedback').textContent = "Correct! Well done."
+        } else {
+            document.querySelector('#feedback').textContent = "That's incorrect. Try again!"
+        }
+    }
 }
+
+document.getElementById('submit-answer').addEventListener('click', checkAnswer) // when the submit button with an id of submit-answer is cliked, it triggers the "click event" executing the checkAnswer callback function
+// make sure not to call the checkAnswer function directly(i.e., do not add "()" after the function name)
+// when using getelementbyId # isn't needed
