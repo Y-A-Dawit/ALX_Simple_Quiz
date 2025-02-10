486e339298c88264c782d85d6bd6e1565ede2ae8
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-02-11 00:32:37 +0300

    ALX_Simple_Quiz

--- quiz.js
+++ quiz.js
@@ -14,4 +14,5 @@ function checkAnswer() {
 
 document.getElementById('submit-answer').addEventListener('click', checkAnswer) // when the submit button with an id of submit-answer is cliked, it triggers the "click event" executing the checkAnswer callback function
 // make sure not to call the checkAnswer function directly(i.e., do not add "()" after the function name)
+// because "()" means function execution, and JS immediately calls checkAnswer() as soon as it reads this line, before the user even clicks the button
 // when using getelementbyId # isn't needed
